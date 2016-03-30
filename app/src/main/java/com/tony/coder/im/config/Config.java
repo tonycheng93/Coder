a907c7849f44ca26a45fd491d2ff3a207f051b97
tonycheng <bc1341286335@gmail.com>
2016-03-30 16:12:16 +0800

    配置一些系统常量

---
+++ app/src/main/java/com/tony/coder/im/config/Config.java
@@ -0,0 +1,16 @@
+package com.tony.coder.im.config;
+
+/**
+ * 项目名称：Coder
+ * 类描述：
+ * 创建人：tonycheng
+ * 创建时间：2016/3/30 16:05
+ * 邮箱：tonycheng93@outlook.com
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+public class Config {
+    //这是Bmob的ApplicationId,用于初始化操作
+    public static String applicationId = "";
+}
---
+++ app/src/main/java/com/tony/coder/im/entity/FaceText.java
@@ -0,0 +1,23 @@
+package com.tony.coder.im.entity;
+
+/**
+ * 项目名称：Coder
+ * 类描述：
+ * 创建人：tonycheng
+ * 创建时间：2016/3/30 15:57
+ * 邮箱：tonycheng93@outlook.com
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+public class FaceText {
+    public String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
