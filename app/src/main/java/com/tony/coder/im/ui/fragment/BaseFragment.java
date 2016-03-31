bead2c8aca84068e21bec54eb3c0cdcdf0e1ec9a
tonycheng <bc1341286335@gmail.com>
2016-03-31 20:49:49 +0800

    自定义一个带清除功能的EditText用于搜索框

---
+++ app/src/main/java/com/tony/coder/im/ui/fragment/BaseFragment.java
@@ -0,0 +1,170 @@
+package com.tony.coder.im.ui.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+import com.tony.coder.R;
+import com.tony.coder.im.CoderApplication;
+import com.tony.coder.im.view.HeaderLayout;
+
+import cn.bmob.im.BmobChatManager;
+import cn.bmob.im.BmobUserManager;
+import cn.bmob.im.util.BmobLog;
+
+/**
+ * 项目名称：Coder
+ * 类描述：
+ * 创建人：tonycheng
+ * 创建时间：2016/3/31 17:28
+ * 邮箱：tonycheng93@outlook.com
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+public class BaseFragment extends Fragment {
+    public CoderApplication mApplication;
+    public BmobChatManager mChatManager;
+    public BmobUserManager mUserManager;
+
+    /**
+     * 公用的Header布局
+     */
+    public HeaderLayout mHeaderLayout;
+    protected View contentView;
+    public LayoutInflater mInflater;
+
+    private Handler mHandler = new Handler();
+
+    public void runOnWorkThread(Runnable action) {
+        new Thread(action).start();
+    }
+
+    public void runOnUiThread(Runnable action) {
+        mHandler.post(action);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        mApplication = CoderApplication.getInstance();
+        mUserManager = BmobUserManager.getInstance(getActivity());
+        mChatManager = BmobChatManager.getInstance(getActivity());
+        mInflater = LayoutInflater.from(getActivity());
+    }
+
+    public BaseFragment() {
+
+    }
+
+    public Toast mToast;
+
+    public void showToast(String text) {
+        if (mToast == null) {
+            mToast = Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT);
+        } else {
+            mToast.setText(text);
+        }
+        mToast.show();
+    }
+
+    public void showToast(int resId) {
+        if (mToast == null) {
+            mToast = Toast.makeText(getActivity(), resId, Toast.LENGTH_SHORT);
+        } else {
+            mToast.setText(resId);
+        }
+        mToast.show();
+    }
+
+    public void showLog(String msg) {
+        BmobLog.i(msg);
+    }
+
+    public View findViewById(int paramInt) {
+        return getView().findViewById(paramInt);
+    }
+
+    /**
+     * 只有title initTopBarLayoutByTitle
+     *
+     * @param titleName
+     */
+    public void initTopBarForOnlyTitle(String titleName) {
+        mHeaderLayout = (HeaderLayout) findViewById(R.id.common_actionbar);
+        mHeaderLayout.init(HeaderLayout.HeaderStyle.DEFAULT_TITLE);
+        mHeaderLayout.setDefaultTitle(titleName);
+    }
+
+    /**
+     * 初始化标题栏-带左右按钮
+     *
+     * @param titleName
+     * @param rightDrawableId
+     * @param listener
+     */
+    public void initTopBarForBoth(String titleName, int rightDrawableId,
+                                  HeaderLayout.onRightImageButtonClickListener listener) {
+        mHeaderLayout = (HeaderLayout) findViewById(R.id.common_actionbar);
+        mHeaderLayout.init(HeaderLayout.HeaderStyle.TITLE_DOUBLE_IMAGEBUTTON);
+        mHeaderLayout.setTitleAndLeftImageButton(titleName,
+                R.drawable.base_action_bar_back_bg_selector,
+                new OnLeftButtonClickListener());
+        mHeaderLayout.setTitleAndRightImageButton(titleName, rightDrawableId,
+                listener);
+    }
+
+    /**
+     * 只有左边按钮和Title initTopBarLayout
+     *
+     * @throws
+     */
+    public void initTopBarForLeft(String titleName) {
+        mHeaderLayout = (HeaderLayout) findViewById(R.id.common_actionbar);
+        mHeaderLayout.init(HeaderLayout.HeaderStyle.TITLE_LIFT_IMAGEBUTTON);
+        mHeaderLayout.setTitleAndLeftImageButton(titleName,
+                R.drawable.base_action_bar_back_bg_selector,
+                new OnLeftButtonClickListener());
+    }
+
+    /**
+     * 右边+title
+     * initTopBarForRight
+     *
+     * @return void
+     * @throws
+     */
+    public void initTopBarForRight(String titleName, int rightDrawableId,
+                                   HeaderLayout.onRightImageButtonClickListener listener) {
+        mHeaderLayout = (HeaderLayout) findViewById(R.id.common_actionbar);
+        mHeaderLayout.init(HeaderLayout.HeaderStyle.TITLE_RIGHT_IMAGEBUTTON);
+        mHeaderLayout.setTitleAndRightImageButton(titleName, rightDrawableId,
+                listener);
+    }
+
+    //左边按钮的点击事件
+    public class OnLeftButtonClickListener implements
+            HeaderLayout.onLeftImageButtonClickListener {
+
+        @Override
+        public void onClick() {
+            getActivity().finish();
+        }
+    }
+
+    /**
+     * 动画启动页面 startAnimActivity
+     * @param intent
+     */
+    public void startAnimActivity(Intent intent){
+        this.startActivity(intent);
+    }
+    public void startAnimActivity(Class<?> clazz){
+        getActivity().startActivity(new Intent(getActivity(),clazz));
+    }
+}
---
+++ app/src/main/java/com/tony/coder/im/ui/fragment/ChatFragment.java
@@ -0,0 +1,29 @@
+package com.tony.coder.im.ui.fragment;
+
+import android.view.View;
+import android.widget.AdapterView;
+
+/**
+ * 项目名称：Coder
+ * 类描述：
+ * 创建人：tonycheng
+ * 创建时间：2016/3/31 17:22
+ * 邮箱：tonycheng93@outlook.com
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+public class ChatFragment extends BaseFragment implements
+        AdapterView.OnItemClickListener, View.OnLongClickListener {
+
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        return false;
+    }
+}
