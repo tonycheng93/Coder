b5a800dfb31a34f6fba3ed5ec662e6dc9d1265fc
tonycheng <bc1341286335@gmail.com>
2016-04-18 21:00:24 +0800

    个人信息修改页面

---
+++ app/src/main/java/com/tony/coder/im/ui/activity/UpdateInfoActivity.java
@@ -0,0 +1,75 @@
+package com.tony.coder.im.ui.activity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.tony.coder.R;
+import com.tony.coder.im.entity.User;
+import com.tony.coder.im.view.HeaderLayout;
+
+import cn.bmob.v3.listener.UpdateListener;
+
+/**
+ * 项目名称：Coder
+ * 类描述：
+ * 创建人：tonycheng
+ * 创建时间：2016/4/15 17:22
+ * 邮箱：tonycheng93@outlook.com
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+public class UpdateInfoActivity extends ActivityBase {
+
+    private EditText edit_nick;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_info);
+
+        initView();
+    }
+
+    private void initView() {
+        initTopBarForBoth("修改昵称", R.drawable.base_action_bar_true_bg_selector,
+                new HeaderLayout.onRightImageButtonClickListener() {
+                    @Override
+                    public void onClick() {
+                        String nick = edit_nick.getText().toString();
+                        if (nick.equals("")) {
+                            showToast("请填写昵称！");
+                            return;
+                        }
+                        updateInfo(nick);
+                    }
+                });
+        edit_nick = (EditText) findViewById(R.id.edit_nick);
+    }
+
+    /**
+     * 修改资料
+     *
+     * @param nick
+     */
+    private void updateInfo(String nick) {
+        final User user = mUserManager.getCurrentUser(User.class);
+        User u = new User();
+        u.setNick(nick);
+        u.setHight(110);
+        u.setObjectId(user.getObjectId());
+        u.update(this, new UpdateListener() {
+            @Override
+            public void onSuccess() {
+                final User c = mUserManager.getCurrentUser(User.class);
+                showToast("修改成功：" + c.getNick() + ",height = " + c.getHight());
+                finish();
+            }
+
+            @Override
+            public void onFailure(int i, String s) {
+                showToast("onFailure:" + s);
+            }
+        });
+    }
+}
