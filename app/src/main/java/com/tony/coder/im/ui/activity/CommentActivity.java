da7154059e86f3bfc40296edde36d4702dd12efe
tonycheng <bc1341286335@gmail.com>
2016-04-29 09:19:35 +0800

    添加评论页面

---
+++ app/src/main/java/com/tony/coder/im/ui/activity/CommentActivity.java
@@ -0,0 +1,14 @@
+package com.tony.coder.im.ui.activity;
+
+/**
+ * 项目名称：Coder
+ * 类描述：
+ * 创建人：tonycheng
+ * 创建时间：2016/4/28 14:56
+ * 邮箱：tonycheng93@outlook.com
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+public class CommentActivity extends BaseActivity{
+}
